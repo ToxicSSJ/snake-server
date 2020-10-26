beffa55fad99787920e2397a6804c04f3852ceaf
Abraham M. Lora <abrahamlora3@gmail.com>
2020-10-25 23:46:40 -0500

    clean

--- target/local_client.sh
+++
@@ -1,2 +0,0 @@
-java -jar snake-server-1.0-SNAPSHOT.jar -p 4000
-read -p "Press [Enter] to exit."
--- target/local_server.sh
+++
@@ -1,2 +0,0 @@
-java -jar snake-server-1.0-SNAPSHOT.jar --server -p 4000 -f "C:/server_data"
-read -p "Press [Enter] to exit."
